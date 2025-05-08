0059fc0976469701c36ec1c5f3d46f70e8aa6b93
Mathew Francisco <mhfrancisco775@gmail.com>
2025-05-08 16:54:56 +0800

    Removed unused import

--- src/main/java/com/mathew/weather/service/WeatherService.java
+++ src/main/java/com/mathew/weather/service/WeatherService.java
@@ -10,7 +10,6 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.scheduling.annotation.Scheduled;
